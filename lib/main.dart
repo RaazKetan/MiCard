6a083085feb8e0a243b694b795841ec7071550c1
Raaz <21ketanraaz@gmail.com>
2023-02-22 11:09:57 +0530

    +changes in container playing with columns and rows

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,38 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
     home:Scaffold(
-    backgroundColor: Colors.teal ,
+    backgroundColor: Colors.teal,
   body: SafeArea(
-    child: Container(
-      height: 100.0,
-      width: 100.0,
-      margin: EdgeInsets.only(left: 30),
-      padding: EdgeInsets.all(20.0),
-      color: Colors.white,
-      child: Text('Lorem'),
-    ),
+    child: Row(
+      // verticalDirection: VerticalDirection.down,
+      // mainAxisSize: MainAxisSize.min,
+      // mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Container(
+          height: 100.0,
+          width: 100.0,
+          color: Colors.white,
+          child: Text('Container 1'),
+        ),
+        SizedBox(
+          width: 20.0,
+        ),
+        Container(width: 100,
+        height: 100.0,
+          color: Colors.blue,
+          child: Text('Container 2'),
+        ),
+        Container(width: 100,
+          height: 100.0,
+          color: Colors.red,
+          child: Text('Container 3'),
+        ),
+      ],
+    )
   ),
   ),
   );
   }
 }
+
