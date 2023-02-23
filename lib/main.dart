f69bcc63e5a48cc307c02f3f42a7dc8a782d4599
Raaz <21ketanraaz@gmail.com>
2023-02-23 18:26:41 +0530

    +flutter layout challenge:Squares

--- lib/main.dart
+++ lib/main.dart
@@ -13,33 +13,82 @@ class MyApp extends StatelessWidget {
     home:Scaffold(
     backgroundColor: Colors.teal,
   body: SafeArea(
-    child: Row(
-      // verticalDirection: VerticalDirection.down,
+    child: Column(
+      // verticalDirection: VerticalDirection.up,
       // mainAxisSize: MainAxisSize.min,
-      // mainAxisAlignment: MainAxisAlignment.spaceAround,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+      // mainAxisAlignment: MainAxisAlignment.start,
       children: <Widget>[
-        Container(
-          height: 100.0,
-          width: 100.0,
-          color: Colors.white,
-          child: Text('Container 1'),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 100.0,
+              width: 300.0,
+              color: Colors.white,
+              child: Text('Container 1'),
+            ),
+            Container(width: 110,
+              height: 100.0,
+              color: Colors.blue,
+              child: Text('Container 2'),
+            ),
+          ],
         ),
-        SizedBox(
-          width: 20.0,
-        ),
-        Container(width: 100,
-        height: 100.0,
-          color: Colors.blue,
-          child: Text('Container 2'),
-        ),
-        Container(width: 100,
-          height: 100.0,
-          color: Colors.red,
-          child: Text('Container 3'),
-        ),
+      Row(
+        children: [
+            Column(
+              // verticalDirection: VerticalDirection.up,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(width: 100,
+                  height: 100.0,
+                  color: Colors.red,
+                  child: Text('Container 4'),
+                ),
+                Container(width: 100,
+                  height: 100.0,
+                  color: Colors.red,
+                ),
+              ],
+            ),
+          Column(
+            children: [
+              Row(
+                verticalDirection: VerticalDirection.up,
+                children: [
+                  Container(width: 200,
+                    height: 100.0,
+                    color: Colors.grey,
+                    child: Text('Container 3'),
+                  ),
+                  Container(width: 110,
+                    height: 100.0,
+                    color: Colors.blue,
+                    // child: Text('Container 5'),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                verticalDirection: VerticalDirection.up,
+                children: [
+                  Container(
+                    width: 310,
+                    height: 100,
+                    color: Colors.green,
+                    child: Text('Container 6'),
+                  )
+                ],
+              )
+
+            ],
+          )
+        ],
+      )
+
+
       ],
-    )
+    ),
   ),
   ),
   );
