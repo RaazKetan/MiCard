6fd0082af751131e61194c40ad9dcda42f561d0c
Raaz <21ketanraaz@gmail.com>
2023-02-24 20:00:57 +0530

    Final Commit +instead of row we replaced it with ListTitle Class and instead of Columns we used Card class

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
     backgroundColor: Colors.teal,
   body: SafeArea(
     child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
        const CircleAvatar(
          radius: 50.0,
@@ -42,52 +43,54 @@ class MyApp extends StatelessWidget {
             letterSpacing: 2.5,
           ),
         ),
-        Container(
-          color: Colors.white,
-          padding: EdgeInsets.all(5.0),
-          margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-          child: Row(
-            children: <Widget>[
-              Icon(
-                Icons.phone,
-                size: 50.0,
-                color: Colors.teal.shade100,
-              ),
-              SizedBox(
-                width: 10.0,
-              ),
-              Text('+91 987654321',
-              style: TextStyle(
-                color: Colors.teal.shade900,
-                fontFamily: 'Source Sans Pro',
-                fontSize: 20.0,
-              ),
-              ),
-            ],
+        SizedBox(
+          height: 20.0,
+          width: 150.0,
+          child: Divider(
+            color: Colors.teal.shade100,
+
           ),
         ),
-        Container(
-          color: Colors.white,
-          padding: EdgeInsets.all(5.0),
+        Card(
+          // color: Colors.white,
           margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-          child: Row(
-            children: <Widget>[
-              Icon(
-                Icons.email,
-                color: Colors.teal.shade100,
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.phone,
                 size: 50.0,
-                  ),
-              SizedBox(
-                width: 10.0,
+                color: Colors.teal.shade900,
               ),
-              Text('21ketanraaz@gmail.com',
+              title: Text('+91 987654321',
+                style: TextStyle(
+                  color: Colors.teal.shade900,
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Card(
+          // color: Colors.white,
+          margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.email,
+                color: Colors.teal.shade900,
+                size: 50.0,
+              ),
+              title: Text('21ketanraaz@gmail.com',
                 style: TextStyle(
                   fontFamily: 'Source Sans Pro',
                   fontSize: 20.0,
                   color: Colors.teal.shade900,
                 ),
               ),
-            ],
+            ),
           ),
         ),
       ],
@@ -98,4 +101,6 @@ class MyApp extends StatelessWidget {
   );
   }
 }
+
+
 
